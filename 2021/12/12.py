05f9ee698147b7482cff29b21a15589f1b47320c
Safian Ali <safian99ali@gmail.com>
2021-12-12 08:19:50 +0000

    day 12 2021

--- 2021/12/12.py
+++ 2021/12/12.py
@@ -1,4 +1,5 @@
 import time
+from collections import defaultdict
 
 start = time.time()
 
@@ -7,7 +8,78 @@ with open("12.txt") as f:
 
 # part 1
 
+caves = {}
+
+for line in rawInput:
+    newCaveStrings = line.split('-')
+
+    for string in newCaveStrings:
+        caves[string] = set()
+
+for line in rawInput:
+    string0, string1 = line.split('-')
+
+    caves[string0].add(string1)
+    caves[string1].add(string0)
+
+for k, v in caves.copy().items():
+    if k == "end":
+        del caves[k]
+    else:
+        caves[k].discard("start")
+
+pathsFound = 0
+
+
+def findPaths(cave, smallCavesVisited):
+    global pathsFound
+    if cave == "end":
+        pathsFound += 1
+        return
+
+    setCopy = smallCavesVisited.copy()
+
+    for connectedCave in caves[cave]:
+        if connectedCave.isupper():
+            findPaths(connectedCave, setCopy)
+        elif connectedCave not in setCopy:
+            setCopy.add(connectedCave)
+            findPaths(connectedCave, setCopy)
+            setCopy.remove(connectedCave)
+
+
+findPaths("start", set())
+
+print("Part 1:", pathsFound)
+
 # part 2
+pathsFound = 0
+
+
+def findPathsPart2(cave, smallCavesVisitedDict, smallCaveVisitedTwice):
+    global pathsFound
+    if cave == "end":
+        pathsFound += 1
+        return
+
+    setCopy = smallCavesVisitedDict.copy()
+
+    for connectedCave in caves[cave]:
+        if connectedCave.isupper():
+            findPathsPart2(connectedCave, setCopy, smallCaveVisitedTwice)
+        elif setCopy[connectedCave] == 0:
+            setCopy[connectedCave] += 1
+            findPathsPart2(connectedCave, setCopy, smallCaveVisitedTwice)
+            setCopy[connectedCave] -= 1
+        elif setCopy[connectedCave] == 1 and not smallCaveVisitedTwice:
+            setCopy[connectedCave] += 1
+            findPathsPart2(connectedCave, setCopy, True)
+            setCopy[connectedCave] -= 1
+
+
+findPathsPart2("start", defaultdict(int), False)
+
+print("Part 2:", pathsFound)
 
 end = time.time()
 print(end - start)
