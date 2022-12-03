a446ecc370ff68768fd3387b5d7475976a6ff623
Safian Ali <safian99ali@gmail.com>
2022-12-03 02:51:40 +0000

    day 2 2022

--- 2022/2/2.py
+++ 2022/2/2.py
@@ -7,7 +7,43 @@ with open("2.txt") as f:
 
 # part 1
 
+scoreDict1 = {
+    "A X": 4,
+    "B X": 1,
+    "C X": 7,
+    "A Y": 8,
+    "B Y": 5,
+    "C Y": 2,
+    "A Z": 3,
+    "B Z": 9,
+    "C Z": 6
+}
+
+totalScore = 0
+for game in rawInput:
+    totalScore += scoreDict1[game]
+
+print(totalScore)
+
 # part 2
+
+scoreDict2 = {
+    "A X": 3,
+    "B X": 1,
+    "C X": 2,
+    "A Y": 4,
+    "B Y": 5,
+    "C Y": 6,
+    "A Z": 8,
+    "B Z": 9,
+    "C Z": 7
+}
+
+totalScore = 0
+for game in rawInput:
+    totalScore += scoreDict2[game]
+
+print(totalScore)
 
 end = time.time()
 print(end - start)
