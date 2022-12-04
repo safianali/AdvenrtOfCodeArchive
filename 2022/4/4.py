c5479db91bd1d30e97fbd8743108affeab6f755e
Safian Ali <safian99ali@gmail.com>
2022-12-04 17:30:09 +0000

    day 4 2022

--- 2022/4/4.py
+++ 2022/4/4.py
@@ -1,3 +1,4 @@
+import re
 import time
 
 start = time.time()
@@ -7,7 +8,23 @@ with open("4.txt") as f:
 
 # part 1
 
+pairs = 0
+for line in rawInput:
+    nums = [int(x) for x in re.findall(r'\d+', line)]
+    if nums[0] <= nums[2] and nums[1] >= nums[3] or nums[0] >= nums[2] and nums[1] <= nums[3]:
+        pairs += 1
+
+print(pairs)
+
 # part 2
+
+pairs = 0
+for line in rawInput:
+    nums = [int(x) for x in re.findall(r'\d+', line)]
+    if nums[0] <= nums[2] <= nums[1] or nums[2] <= nums[0] <= nums[3]:
+        pairs += 1
+
+print(pairs)
 
 end = time.time()
 print(end - start)
