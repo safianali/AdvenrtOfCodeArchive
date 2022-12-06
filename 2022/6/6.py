da00d7714830aae39fed704e32233361e2ca6851
Safian Ali <safian99ali@gmail.com>
2022-12-06 23:02:01 +0000

    day 6 2022

--- 2022/6/6.py
+++ 2022/6/6.py
@@ -1,3 +1,4 @@
+import collections
 import time
 
 start = time.time()
@@ -5,9 +6,38 @@ start = time.time()
 with open("6.txt") as f:
     rawInput = f.read().splitlines()
 
+
 # part 1
+def isSOP(x):
+    charSet = set(x)
+    return len(charSet) == len(x)
+
+
+def findMarker(message, numUnique):
+    curIdx = numUnique
+    initChars = [x for x in message[0:curIdx]]
+    buffer = collections.deque(initChars)
+    found = False
+
+    for char in message[curIdx:]:
+        if isSOP(buffer):
+            print(curIdx)
+            found = True
+            break
+
+        curIdx += 1
+        buffer.popleft()
+        buffer.append(char)
+
+    if not found:
+        print(curIdx)
+
+
+findMarker(rawInput[0], 4)
 
 # part 2
+
+findMarker(rawInput[0], 14)
 
 end = time.time()
 print(end - start)
