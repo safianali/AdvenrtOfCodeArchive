7062895d0a3c52a5318b97fce3dc8a72e4135514
Safian Ali <safian99ali@gmail.com>
2024-12-06 01:07:05 +0000

    day 1 2024

--- 2024/1/1.py
+++ 2024/1/1.py
@@ -1,4 +1,5 @@
 import time
+from collections import Counter
 
 start = time.time()
 
@@ -7,7 +8,30 @@ with open("1.txt") as f:
 
 # part 1
 
+list1, list2 = [], []
+for line in rawInput:
+    split = line.split()
+    list1.append(int(split[0]))
+    list2.append(int(split[1]))
+
+list1.sort()
+list2.sort()
+
+diff = 0
+for num1, num2 in zip(list1, list2):
+    diff += abs(num1 - num2)
+
+print("Part 1:", diff)
+
 # part 2
+
+list2counter = Counter(list2)
+
+sim_score = 0
+for num1 in list1:
+    sim_score += num1 * list2counter[num1]
+
+print("Part 2:", sim_score)
 
 end = time.time()
 print(end - start)
