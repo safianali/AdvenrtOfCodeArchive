e4fc4c2b2b0385c477f70950b29f158ad9326c99
Safian Ali <safian99ali@gmail.com>
2024-12-08 01:04:09 +0000

    day 5 2024

--- 2024/5/5.py
+++ 2024/5/5.py
@@ -1,4 +1,5 @@
 import time
+from collections import defaultdict
 
 start = time.time()
 
@@ -7,7 +8,74 @@ with open("5.txt") as f:
 
 # part 1
 
+i = 0
+before = defaultdict(set)
+while rawInput[i] != "":
+    beforeNum, afterNum = [int(x) for x in rawInput[i].split("|")]
+    before[beforeNum].add(afterNum)
+
+    i += 1
+
+i += 1
+firstUpdateIdx = i
+
+middleSum = 0
+while i < len(rawInput):
+    update = [int(x) for x in rawInput[i].split(",")]
+    
+    seen = set()
+    correct = True
+    
+    for page in update:
+        if not correct:
+            break
+
+        for beforePage in before[page]:
+            if beforePage in seen:
+                correct = False
+                break
+        
+        seen.add(page)
+
+    if correct:
+        middleSum += update[len(update) // 2]
+        
+    i += 1
+
+print("Part 1:", middleSum)
+
 # part 2
+
+middleSum = 0
+i = firstUpdateIdx
+while i < len(rawInput):
+    update = [int(x) for x in rawInput[i].split(",")]
+
+    seen = set()
+    correctedUpdate = False
+    
+    for j in range(len(update)):
+        seen.add(update[j])
+        correctedPage = False
+        for beforePage in before[update[j]]:
+            if beforePage in seen:
+                correctedPage = True
+                break
+
+        if correctedPage:
+            correctedUpdate = True
+            for k in range(len(update)):
+                if update[k] in before[update[j]]:
+                    update.insert(k, update.pop(j))
+                    break
+
+    if correctedUpdate:
+        middleSum += update[len(update) // 2]
+        
+    i += 1
+    
+print("Part 2:", middleSum)
+
 
 end = time.time()
 print(end - start)
