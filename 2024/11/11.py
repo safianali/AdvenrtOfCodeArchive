8ad5f7d0a28dce34b6eeea70c1a185ac2bfcd52b
Safian Ali <safian99ali@gmail.com>
2024-12-15 23:51:01 +0000

    day 11 2024

--- 2024/11/11.py
+++ 2024/11/11.py
@@ -1,4 +1,5 @@
 import time
+from collections import Counter
 
 start = time.time()
 
@@ -7,7 +8,45 @@ with open("11.txt") as f:
 
 # part 1
 
+def getNewStone(stoneNum):
+    match stoneNum:
+        case 0:
+            return [1]
+        case n if len(str(n)) % 2 == 0:
+            numStr = str(n)
+            mid = len(numStr) // 2
+            first_half = int(numStr[:mid])
+            second_half = int(numStr[mid:])
+            return (first_half, second_half)
+        case _:
+            return [stoneNum * 2024]
+
+stones = Counter(map(int, rawInput[0].split()))
+stonesCopy = stones.copy()
+
+for i in range(25):
+    newStones = Counter()
+
+    for stone, count in stones.items():
+        for newStone in getNewStone(stone):
+            newStones[newStone] += count
+
+    stones = newStones
+
+print("Part 1:", stones.total())
+
 # part 2
+
+for i in range(75):
+    newStones = Counter()
+
+    for stone, count in stonesCopy.items():
+        for newStone in getNewStone(stone):
+            newStones[newStone] += count
+
+    stonesCopy = newStones
+
+print("Part 2:", stonesCopy.total())
 
 end = time.time()
 print(end - start)
